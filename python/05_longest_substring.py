6930878993d38f53524ae42451abc86a9cfdeea6
ayush shrestha <ayyush227@gmail.com>
2026-01-17 00:47:26 +0545

    05_longest_substring

--- python/05_longest_substring.py
+++ python/05_longest_substring.py
@@ -1,14 +1,16 @@
 def longestSubstring(s:str)->int:
-    if not s:
-        return 0
-    
-    char_index_map={}
+    charSet=set() #set removes the extra repeating numbers in a list
+    l=0
+    res=0
 
-    left=0
-    max_length=0
+    for r in range(len(s)):   #len=8 r=0, r=1,r=2 ,r=3
+        print(f"r:{r}") #0 , 1 ,2 ,3
+        while s[r] in charSet: #s=a , s=b , s=c ,s=a (true)
+            charSet.remove(s[l]) #skip,#skip,#skip , l=0 -> charset(a remove)
+            l+=1 #skip ,#skip,#skip , l=1
+        print(f"man s[r] :{s[r]}")
+        charSet.add(s[r])   #a charset=a ,charset =b, charset=c , charset=a
+        res=max(res,r-l+1) #max(0,0-0+1) , max(1,1-0+1), max(2,2-0+1) , max(3,3-1 +1)
+        print("res:",res) #1,2,3,3,
 
-    for right in range(len(s)):
-        char=s[right]
-
-        if char in char_index_map and char_index_map[char] >=left:
-            left = char_index_map[char] + 1 
+longestSubstring("abcabc")
